48f2a96db72245d2e90dd67f0e4cfe0c3f98f55a
unknown <dangabarin2020@gmail.com>
2026-07-20 15:16:43 +0100

    update commit

--- src/pages/account/index.js
+++ src/pages/account/index.js
@@ -1,8 +1,36 @@
-export default function Account() {
+import { getCustomer } from "@/utils/customer";
+
+function parseCookies(cookieHeader = '') {
+  return Object.fromEntries(
+    cookieHeader.split('; ').map((c) => {
+      const [key, ...v] = c.split('=');
+      return [key, v.join('=')];
+    })
+  );
+}
+
+export default function Account({ customer }) {
   return (
     <div style={{ padding: '2rem' }}>
       <h1>My Account</h1>
-      <p>You are successfully logged in with Shopify Customer Accounts.</p>
+
+      {customer ? (
+        <>
+          <p>
+            <strong>Name:</strong> {customer.firstName}{' '}
+            {customer.lastName}
+          </p>
+
+          <p>
+            <strong>Email:</strong>{' '}
+            {customer.emailAddress.emailAddress}
+          </p>
+        </>
+      ) : (
+        <p>Could not load customer data.</p>
+      )}
+
+      <hr />
 
       <a href="/account/orders">My Orders</a>
       <br />
@@ -11,4 +39,34 @@ export default function Account() {
       <a href="/api/auth/logout">Logout</a>
     </div>
   );
+}
+
+export async function getServerSideProps({ req }) {
+  const cookies = parseCookies(req.headers.cookie || '');
+  const token = cookies.customer_access_token;
+
+  if (!token) {
+    return {
+      redirect: {
+        destination: '/account/login',
+        permanent: false,
+      },
+    };
+  }
+
+  try {
+    const { data } = await getCustomer(token);
+
+    return {
+      props: {
+        customer: data.customer,
+      },
+    };
+  } catch (error) {
+    return {
+      props: {
+        customer: null,
+      },
+    };
+  }
 }
